b14b167813a9e6a69aac7bef62bd51d32b1cde30
mitrich <4irtim@gmail.com>
2023-10-04 22:14:15 +0300

    feat: add text editor container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import './App.css';
+import Editor from './editor/Editor';
 
 export default function App(): JSX.Element {
-  return <main className="container">TEST</main>;
+  return (
+    <main className="container">
+      <Editor />
+    </main>
+  );
 }
---
+++ src/editor/Editor.tsx
@@ -0,0 +1,5 @@
+import './Editor.css';
+
+export default function Editor(): JSX.Element {
+  return <section className="editor__container">TEST</section>;
+}
