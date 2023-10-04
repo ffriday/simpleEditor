f775740c84c700df84fd34b02cfc978bb7c02718
mitrich <4irtim@gmail.com>
2023-10-04 16:14:27 +0300

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
   ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     tsconfigRootDir: __dirname,
-    project: 'tsconfig.json'
+    project: 'tsconfig.json',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/semi': 0,
     'react/react-in-jsx-scope': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/triple-slash-reference': 0
-  }
-}
+    '@typescript-eslint/triple-slash-reference': 0,
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '../public/vite.svg';
 import './App.css';
 
-function App (): JSX.Element {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,24 +18,23 @@ function App (): JSX.Element {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button type="button" onClick={() => { setCount((counter) => counter + 1) }}>
-          count is
-          {' '}
-          {count}
+        <button
+          type="button"
+          onClick={() => {
+            setCount((counter) => counter + 1);
+          }}
+        >
+          count is {count}
         </button>
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
