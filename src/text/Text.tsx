f1bad7f2b451815ef30b4a491b072dc61090c2f1
mitrich <4irtim@gmail.com>
2023-10-06 15:01:17 +0300

    feat: add editor line hilight

--- src/text/Text.tsx
+++ src/text/Text.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './Text.css';
 
 export default function Text(): JSX.Element {
+  const [content, setContent] = useState('');
+  console.log(content);
   return (
-    <section className="editor__text" contentEditable="true">
-      TEXT
+    <section
+      className="editor__text"
+      contentEditable="true"
+      onInput={(event) => {
+        setContent(event.currentTarget.innerHTML ?? '');
+      }}
+    >
+      TEST
     </section>
   );
 }
