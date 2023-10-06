4a03995c38cc2cd99a593bb5424c9fc3dc7610a5
mitrich <4irtim@gmail.com>
2023-10-06 20:33:42 +0300

    feat: text input and paste handling (without positioning)

--- src/text/Text.tsx
+++ src/text/Text.tsx
@@ -1,18 +1,43 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import ContentEditable, {
+  type ContentEditableEvent,
+} from 'react-contenteditable';
 import './Text.css';
 
+// TODO:
+// Handle tab
+
 export default function Text(): JSX.Element {
-  const [content, setContent] = useState('');
-  console.log(content);
+  const [refresh, setRefresh] = useState(false);
+  const content = useRef('');
+
+  const pasteHandler = (event: React.ClipboardEvent<HTMLDivElement>): void => {
+    event.preventDefault();
+    const text = event.clipboardData
+      .getData('text')
+      .split('\n')
+      .map((el) => `<div>${el}</div>`)
+      .join('');
+    content.current = `${content.current}${text}`;
+    setRefresh(!refresh);
+    console.log(content.current, text);
+  };
+
+  const handleChange = (event: ContentEditableEvent): void => {
+    let input = event.target.value;
+    const firstTag = input.indexOf('<');
+    if (firstTag === -1) input = `<div>${input}</div>`;
+    content.current = input;
+    console.log(firstTag, input, content);
+  };
+
   return (
-    <section
+    <ContentEditable
       className="editor__text"
-      contentEditable="true"
-      onInput={(event) => {
-        setContent(event.currentTarget.innerHTML ?? '');
-      }}
-    >
-      TEST
-    </section>
+      html={content.current}
+      onChange={handleChange}
+      onInput={handleChange}
+      onPaste={pasteHandler}
+    />
   );
 }
